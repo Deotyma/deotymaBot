36797e0a4da1b4d7b8561b2c6c26b07275b13391
Ewa Kadziolka <kalistaewa@gmail.com>
2022-05-25 21:38:52 +0200

    acces to tweeter

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+console.log('Bot is working')
+
+const Twit = require('twit');
+const axios = require('axios').default; 
+
+var T = new Twit({
+    consumer_key: process.env.TWITTER_API_KEY,
+    consumer_secret: process.env.TWITTER_API_SECRET,
+    access_token: process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  })
+
+ /*  T.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
+    console.log(data)
+  }) */
+
